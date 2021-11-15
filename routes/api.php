6092b9161bf39c3b28711989a5bb5607854a2f98
Jorge Rodrigo Torrez Aramayo <jorgerodrigotorrez@gmail.com>
2021-11-15 03:22:52 +0000

    user route resource created

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->group(function () {
-    Route::get('echo', function(){
+    Route::get('echo', function () {
         return "Cambasoft Web Services running...";
     });
+
+    Route::apiResources([
+        'users' => UserController::class,
+    ]);
 });
