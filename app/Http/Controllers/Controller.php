b9076339e2642cc2cdf038e65de9ed1cc07940a0
Jorge Rodrigo Torrez Aramayo <jorgerodrigotorrez@gmail.com>
2021-11-15 03:21:58 +0000

    base controller modified with used responses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,40 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function responseOk($data)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Done.',
+            'data' => $data
+        ], 200);
+    }
+
+    public function responseCreated($data)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Created Successfully.',
+            'data' => $data
+        ], 201);
+    }
+
+    public function responseBadRequest($data)
+    {
+        return response()->json([
+            'status' => 'warning',
+            'message' => 'Wrong Information.',
+            'data' => $data
+        ], 400);
+    }
+
+    public function responseNotFound($data)
+    {
+        return response()->json([
+            'status' => 'warning',
+            'message' => 'Not Found.',
+            'data' => $data
+        ], 404);
+    }
 }
